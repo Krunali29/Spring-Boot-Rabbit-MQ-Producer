d76cc61ca118780540c38eecfb9e33711b3742f8
Krunali <krunalilole29@gmail.com>
2022-07-03 18:56:19 +0530

    Added Spring Boot Rabbit MQ Producer Project to get the message in the queue

---
+++ springrabbitmqproducer/springrabbitmqproducer/src/test/java/com/bridgelabz/springrabbitmqproducer/SpringRabbitMqProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.springrabbitmqproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRabbitMqProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
